4ef09d0d005c539ecd9180b762b4a8eb609300d8
Anttam <96652586+Anttam@users.noreply.github.com>
2024-04-16 14:28:04 -0400

    remove dotenv

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 
